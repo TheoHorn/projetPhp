d75389e67cfc7610f9bf65f63ac22d5666b979e3
TheoHorn <theo.hornberger@gmail.com>
2021-12-14 09:22:25 +0100

    Update ItemController.php

--- src/controleur/ItemController.php
+++ src/controleur/ItemController.php
@@ -2,12 +2,13 @@
 
 namespace wishlist\controller;
 use wishlist\vue\VueItem;
+use wishlist\vue\VueParticipant;
 
 class ItemController {
 
     public function getItem($rq, $rs,$args){
         $item = Item::where('id', '=',$args['id'])->first();
-        $v = new VueItem($item, ITEM_VIEW);
+        $v = new VueParticipant($item, ITEM_VIEW);
         $rs->getBody()->write($v->render());
 
         return $rs;
