1bcea6f858dbce76b1bd758e7510f7ef0021ed17
TheoHorn <theo.hornberger@gmail.com>
2022-01-04 09:27:11 +0100

    maj

--- essai.php
+++ essai.php
@@ -1,8 +1,25 @@
 <?php
 require_once __DIR__. '/src/conf/Database.php';
 require_once __DIR__. '/src/vendor/autoload.php' ;
-use wishlist\modele\Item;
 
-$l = Item::all();
-echo $l->toJson();
+use wishlist\modele\Item;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$c = new \Slim\Container(["settings"=>[
+    "displayErrorDetails" => true]]);
+$app = new \Slim\App($c);
+
+$app->get('/hello/{name}[/]',function(Request $rq, Response $rs, array $args): Response{
+
+    $name = $args['name'];
+    $l = Item::all();
+    echo $l->toJson();
+    $rs->getBody()->write("<h1>Hello world, $name</h1>");
+    return $rs;
+    }
+);
+
+
+$app->run();
 
