b50c616acc2abeb31532a38043bc20a16cf6ae59
TheoHorn <theo.hornberger@gmail.com>
2022-01-10 10:26:03 +0100

    correctif

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use wishlist\conf\Database;
+use wishlist\controleur as Control;
 
 require_once __DIR__ . '/src/vendor/autoload.php';
 
@@ -13,35 +14,35 @@ $c = new \Slim\Container(["settings"=>[
 $app= new \Slim\App($c);
 
 $app->get('/', function(Request $rq, Response $rs, array $args): Response {
-    return (new \wishlist\controleur\Controleur)->acceuil($rq,$rs,$args);
+    return (new Control\Controleur)->acceuil($rq,$rs,$args);
 });
 
 $app->get('/item', function(Request $rq, Response $rs, array $args): Response {
-    return (new wishlist\controleur\ItemControleur)->afficheItems($rq,$rs,$args);
+    return (new Control\ItemControleur)->afficheItems($rq,$rs,$args);
 });
 
 $app->get('/item/{id}', function(Request $rq, Response $rs, array $args): Response {
-    return (new wishlist\controleur\ItemControleur)->afficheItem($rq,$rs,$args);
+    return (new Control\ItemControleur)->afficheItem($rq,$rs,$args);
 });
 
 $app->get('/liste', function(Request $rq, Response $rs, array $args): Response {
-    return (new \wishlist\controleur\ListeControleur)->afficherListes($rq,$rs,$args);
+    return (new Control\ListeControleur)->afficherListes($rq,$rs,$args);
 });
 
 $app->get('/liste/new', function(Request $rq, Response $rs, array $args): Response {
-    return (new \wishlist\controleur\ListeControleur)->nouvelleListe($rq,$rs,$args);
+    return (new Control\ListeControleur)->nouvelleListe($rq,$rs,$args);
 });
 
 $app->get('/liste/{token}', function(Request $rq, Response $rs, array $args): Response {
-    return (new \wishlist\controleur\ListeControleur)->afficherListe($rq,$rs,$args);
+    return (new Control\ListeControleur)->afficherListe($rq,$rs,$args);
 });
 
 $app->get('/Connection', function(Request $rq, Response $rs, array $args): Response {
-    return (new \wishlist\controleur\Controleur)->seConnecter($rq,$rs,$args);
+    return (new Control\Controleur)->seConnecter($rq,$rs,$args);
 });
 
 $app->get('/Inscription', function(Request $rq, Response $rs, array $args): Response {
-    return (new \wishlist\controleur\Controleur)->inscription($rq,$rs,$args);
+    return (new Control\Controleur)->inscription($rq,$rs,$args);
 });
 
 
