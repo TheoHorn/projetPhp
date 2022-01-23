0fa7bc406ae0219e0bd671bad309f269c720f2bf
Maxime-MONZA <Maxime-MONZA@users.noreply.github.com>
2022-01-23 18:24:55 +0100

    ajout affichage modif nom d'utilisateur + modif mot de passe

--- src/vue/VueMembre.php
+++ src/vue/VueMembre.php
@@ -11,6 +11,9 @@ class VueMembre extends Vue
     const LIST_VIEW = 2;
     const ACCEUIL = 4;
     const MY_LISTS_VIEW = 3;
+    const MODIF_ID = 5;
+    const MODIF_PASS = 6;
+    const MY_ACCOUNT = 7;
 
     private function acceuil()
     {
@@ -19,7 +22,8 @@ class VueMembre extends Vue
         $rs .= '<div><p>Bonjour '.$_SESSION['username'].', vous etes bien connecter</p></div>';
         $rs .= "<p><a href='$urllist'>Listes publiques</a></p>";
         $rs .= "<p><a href='mesListes'>Mes Listes</a></p>";
-        $rs .= "<p><a href='$urlcreat'>Créateurs</a></p>";
+        $rs .= "<p><a href='./createurs'>Créateurs</a></p>";
+        $rs .= "<p><a href='./monCompte'>Mon Compte</a></p>";
         $rs .= '<div>
                     <h2>Acceder à une liste</h2>
                     <form method="post" action="">
@@ -128,6 +132,15 @@ class VueMembre extends Vue
             case self::MY_LISTS_VIEW :
                 $content = $this->affichageMesListes();
                 break;
+            case self::MY_ACCOUNT :
+                $content = $this->monCompte();
+                break;
+            case self::MODIF_PASS :
+                $content = $this->modifPass();
+                break;
+            case self::MODIF_ID :
+                $content = $this->modifId();
+                break;
             default :
                 break;
         }
@@ -146,6 +159,87 @@ class VueMembre extends Vue
                 END ;
         return $html;
 
+    }
+
+    private function monCompte()
+    {
+        $rs='<div align="center"><h1>Mon Compte</h1>
+                <div>
+                    <label>Mon identifant : </label>'.$_SESSION['username'].' 
+                     <a href="./username">Modifier mon identifiant</a>
+                </div>
+                <a href="./password">Modifier mon mot de passe</a></div>';
+        return $rs;
+    }
+
+    private function modifPass()
+    {
+        $rs='<div align="center">
+                    <h2>Modification du mot de passe</h2>
+                    <br><br><br>
+                    <form method="post" action="">
+                        <table>
+                            <tr>
+                                <td>
+                                    <label>Ancien mot de passe :</label>
+                                </td>
+                                <td>
+                                    <input type="password" placeholder="Votre ancien mot de passe" name="ancpassword" pattern=".[a-zA-z&0-9_!]{8,20}" required>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Nouveau mot de passe :</label>
+                                </td>
+                                <td>
+                                    <input type="password" placeholder="Votre nouveau mot de passe" name="newpassword" pattern=".[a-zA-z&0-9_!]{8,20}" required>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Confirmer votre nouveau mot de passe :</label>
+                                </td>
+                                <td>
+                                    <input type="password" placeholder="Votre nouveau mot de passe" name="repnewpassword" pattern=".[a-zA-z&0-9_]{8,20}" required>
+                                </td>
+                            </tr>
+                        </table>
+                        <br>
+                        <input type="submit" name="modifpass" value="Je change"/>
+                    </form>
+                </div>';
+        return $rs;
+    }
+
+    private function modifId()
+    {
+        $rs='<div align="center">
+                    <h2>Inscription</h2>
+                    <br><br><br>
+                    <form method="post" action="">
+                        <table>
+                            <tr>
+                                <td>
+                                    <label>Identifiant :</label>
+                                </td>
+                                <td>
+                                     <label>'.$_SESSION['username'].'</label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Nouvel identifiant :</label>
+                                </td>
+                                <td>
+                                    <input type="text" placeholder="Votre nouvel identifiant" name="newId" pattern=".[a-zA-z0-9]{3,20}" required>
+                                </td>
+                            </tr>
+                        </table>
+                        <br>
+                        <input type="submit" name="valider" value="Je change"/>
+                    </form>
+                </div>';
+        return $rs;
     }
 
 
